3096270596dca2f84f9da923aa9e4c3957927de1
Amalcxc <amalcxirsi@gmail.com>
2021-03-16 20:24:56 +0300

    my_select

--- enumerables.rb
+++ enumerables.rb
@@ -15,10 +15,27 @@ module Enumerable
     end
   end
 
+  def my_select
+    i = 0
+    fliter = []
+    while i < self.length
+      fliter.push(self[i]) if yield(self[i])
+      i += 1
+    end
+    fliter
+  end  
+
 end
 
 ["A","b","A","b","A","b"].my_each{|a| puts a}
 
 ["A","b","A","b","A","b"].my_each_with_index{
-  |num| puts "#{num} -> #{i = 'potato' if i > 2}"
-}
+  |num| puts "#{num} -> #{i = 'potato' if i > 2}"}
+
+["A","b","A","john","A","b"].my_select{
+  |person| person != "john"}
+
+
+
+
+
