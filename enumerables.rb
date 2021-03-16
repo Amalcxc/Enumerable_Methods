b80cedf277f6d767b5909b2b63c54b61507b014b
Fernando R P Marques <frpm1001@gmail.com>
2021-03-16 13:51:01 -0300

    Add #my_each_with_index method

--- enumerables.rb
+++ enumerables.rb
@@ -1,13 +1,24 @@
 module Enumerable
   def my_each
-    # your code here
     i = 0
-    #last_arr = self.length -1
     while i < self.length 
       yield(self[i])
       i += 1 
     end  
   end
+
+  def my_each_with_index
+    i = 0
+    while i < self.length
+      yield(self[i], i)
+      i += 1
+    end
+  end
+
 end
 
 ["A","b","A","b","A","b"].my_each{|a| puts a}
+
+["A","b","A","b","A","b"].my_each_with_index{
+  |num| puts "#{num} -> #{i = 'potato' if i > 2}"
+}
