b64c9e92bf9028f6d354648a5e9c0cff23cd2db3
Amalcxc <amalcxirsi@gmail.com>
2021-03-18 00:50:33 +0300

    fixed the test page

--- test.rb
+++ test.rb
@@ -1,26 +1,34 @@
-factor = proc { |n| n * 2 }
-
-puts [2, 3, 4].my_map(&factor)
-
-puts ([1, 2].my_map_proc { |n| n * 2 }).to_s
-
-puts [2, 20, 5].my_inject(:*)
-
-puts [3, 6, 10].my_inject(0) { |sum, number| sum + number }
-
+#my_each
 puts :+.is_a?(String)
 %w[A b A b A b].my_each { |a| puts a }
+
+#my_each_with_index
 %w[A b A b A b].my_each_with_index do |num|
   puts "#{num} -> #{'potato' if i > 2}"
 end
 
+#my_select
 %w[A b A john A b].my_select do |person|
   person != 'john'
 end
 
+#my_all
 %w[foiiur fiiive sixsix seveeen].my_all? { |a| a.length > 5 }
 
+#my_any
 %w[fr fiiive sixsix seveeen].my_any? { |a| a.length > 5 }
 
+#my_none
 %w[fix emmm wdss22 ssss].my_none? { |a| a.length > 5 }
+
+#my_count
 puts [1, 1, 1, 4, nil, nil].my_count(1, 2, 3)
+
+#my_map 
+factor = proc { |n| n * 2 }
+puts [2, 3, 4].my_map(&factor)
+puts ([1, 2].my_map_proc { |n| n * 2 }).to_s
+
+#my_inject
+puts [2, 20, 5].my_inject(:*)
+puts [3, 6, 10].my_inject(0) { |sum, number| sum + number }
